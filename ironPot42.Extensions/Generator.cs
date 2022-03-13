9395cd0807f32405d452dee2ac5ab1c2a1ef3584
shockz <shockzinfinity@gmail.com>
2022-03-13 17:17:08 +0900

    add mongo object id generator

---
+++ ironPot42.Extensions/Generator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ironPot42.Extensions
+{
+  public static class Generator
+  {
+    public static string MongoObjectId()
+    {
+      Int64 val = 0;
+      var st = new DateTime(1970, 1, 1);
+      TimeSpan t = (DateTime.Now.ToUniversalTime() - st);
+      val = (Int64)(t.TotalMilliseconds + 0.5);
+      char[] startVal = val.ToString("X").ToCharArray();
+      string timestamp = "";
+      for (int i = 0; i < 10; i++)
+      {
+        timestamp += startVal[i];
+      }
+      int num = 0;
+      Random rand = new Random();
+      for (int i = 0; i < 14; i++)
+      {
+        num = rand.Next(1, 16);
+        timestamp += num.ToString("X");
+      }
+      return timestamp.ToLower();
+    }
+  }
+}
